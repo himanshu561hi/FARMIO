b6fe504583b6da98468f3f5f9811d176e9451796
AMAN GUPTA <ag0567688@gmail.com>
2025-11-23 21:57:04 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -19,25 +19,7 @@ const io = socketIo(server, { cors: { origin: '*' } });
 
 connectDB();
 
-//app.use(cors("*"));
-const allowedOrigins = [
-  'http://localhost:5173', // Local development
-  'https://agrifarmio.netlify.app', // Your actual Netlify domain
-  'http://127.0.0.1:5173'
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
+app.use(cors("*"));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'Uploads')));
 
